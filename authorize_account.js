5d890638daa90ff494bc60ddc24834071d85a86d
space <55336451+spacebytee@users.noreply.github.com>
2025-10-05 22:41:43 -0400

    account finalization

---
+++ authorize_account.js
@@ -0,0 +1,27 @@
+import { endpoints } from '/api_endpoints.js';
+
+let url = new URL(window.location.href);
+let token = url.searchParams.get("token");
+
+if (!token) {
+    window.location = "/index";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetch(endpoints.FINALIZE, {
+        method: "POST",
+        headers: {"Content-Type": "text/plain"},
+        body: token
+    })
+    .then(async (response) => {
+        if (response.ok) {
+          document.getElementById("authstatus").innerHTML = "congratulations! your account has successfully been created!";
+        } else {
+            let em = await response.text();
+            throw new Error(em);
+        }
+    })
+    .catch((error) => {
+        document.getElementById("authstatus").innerHTML = error.message;
+    });
+});
